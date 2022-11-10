8e02e610395c7179de6593e76b5741ca954aa718
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-11-10 18:41:33 -0500

    Creación de archivos Task

---
+++ src/pure/forms/task.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Proptypes from 'prop-types';
+import { Task } from "../../models/task.class";
+
+const TaskComponent = ({ task }) =>{
+    return (
+        <div>
+            <h2>
+                Nombre: { task.name }
+            </h2>
+            <h3>
+                Descripción: {task.description }
+            </h3>
+            <h3>
+                Level:  {task.level}
+            </h3>
+            <h4>
+                La tarea esta: { task.completed ? 'COMPLETADA': 'PENDIENTE'}
+            </h4>
+        </div>
+    )
+};
+
+
+TaskComponent.protoTypes ={
+    task: Proptypes.instanceOf(Task)
+}; 
