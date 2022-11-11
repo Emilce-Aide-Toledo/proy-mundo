1f14812eb7f2482e85dd2356b385d189bc5c90bd
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-11-10 21:50:33 -0500

    Hooks: useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import TaskListComponent from './components/containers/task_list';
+import EjemploState from './hooks/useState';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
      <TaskListComponent></TaskListComponent>
+     {/*Ejemplo de HOOK useState*/}
+     <EjemploState></EjemploState>
       </header>
     </div>
   );
