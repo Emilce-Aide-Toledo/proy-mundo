c889f0dda07171f8e6df32c9a301ddaa2812684f
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-11-11 19:23:23 -0500

    ciclos de vida

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-// import TaskListComponent from './components/containers/task_list';
-// import EjemploState from './hooks/EjemploState';
+import TaskListComponent from './components/containers/task_list';
+import EjemploState from './hooks/EjemploState';
 import EjemploEffect from './hooks/EjemploEffect';
 import { EjemploContext } from './hooks/EjemploContext';
 import EjemploChildren from './hooks/EjemploChildren';
@@ -11,11 +11,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     {/* <TaskListComponent></TaskListComponent> */}
+     <TaskListComponent></TaskListComponent>
      {/*Ejemplo de HOOK useState*/}
-     {/* <EjemploState></EjemploState> */}
-     {/* <EjemploEffect></EjemploEffect> */}
-     {/* <EjemploContext></EjemploContext> */}
+     <EjemploState></EjemploState>
+     <EjemploEffect></EjemploEffect>
+     <EjemploContext></EjemploContext>
      <EjemploChildren name = 'Emilce'>
       <h3>
         Contenido de props.children
