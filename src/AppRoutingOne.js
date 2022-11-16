4517eb8d0b31cddcf7d30d4aa52a4977bc7e5394
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-11-16 18:10:23 -0500

    peticiones con Axios

--- src/AppRoutingOne.js
+++ src/AppRoutingOne.js
@@ -9,6 +9,8 @@ import ProfilePage from './pages/profile/ProfilePage';
 import LoginPage from './pages/auth/LoginPage';
 // import Dashboard from './pages/home/Dashboard';
 import FetchExample from './components/pure/forms/FetchExample';
+import AxiosExample from './components/pure/forms/AxiosExample';
+// import AxiosExample from './components/pure/forms/AxiosExample';
 
 function AppRoutingOne() {
 
@@ -42,10 +44,12 @@ function AppRoutingOne() {
           <Link to='/task/1'> | TASK 1 | </Link>
           <Link to='/users'> | Users | </Link>
           <Link to='/login'> | LOGIN | </Link>
+          <Link to='/axios'> | Axios | </Link>
         </aside>
         <main>
         <Routes>
         <Route exact path='/' element= {<HomePage/>}></Route>
+        <Route exact path='/axios' element= {<AxiosExample/>}></Route>
         <Route path='/profile' element={<ProfilePage/>}>
           {/* {
             logged ?
